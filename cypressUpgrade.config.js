9a2f00c8ea03bc2fdfd8984a25e23fb2aecf6577
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-24 11:32:17 +0300

    Add files via upload

---
+++ cypressUpgrade.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  // These settings apply everywhere unless overridden
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  // Viewport settings overridden for component tests
+  component: {
+    viewportWidth: 500
+  },
+  specs: {
+    defaultCommandTimeout: 10000
+  }
+})
