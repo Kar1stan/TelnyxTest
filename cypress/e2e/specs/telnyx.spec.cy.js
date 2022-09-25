22b06cb9416f288205b36a152aeab3ceaf644e64
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-25 19:10:19 +0300

    Update telnyx.spec.cy.js

--- cypress/e2e/specs/telnyx.spec.cy.js
+++ cypress/e2e/specs/telnyx.spec.cy.js
@@ -4,54 +4,54 @@ import ProductsPage from "../../pageobjects/Products.page"
 
 describe('Telnyx Test', () => {
   it('Should SignUp with valid credentials', () => {
-    SignUpPage.signUpWithValidCredentials("zixter28@gmail.com","boroQin","#Y*vz.102wh*90d");
+    SignUpPage.fillSignUpWithValidCredentials("zixter28@gmail.com","boroQin","#Y*vz.102wh*90d");
     cy.wait(15000)
     cy.url().should('eq','https://telnyx.com/sign-up/verify-email/b');
   })
 
   it('Should SignUp with invalid password', () => {
-    SignUpPage.signUpWithInvalidPassword("dfffg1QzBBBqq");
+    SignUpPage.fillSignUpWithInvalidPassword("dfffg1QzBBBqq");
     cy.url().should('eq','https://telnyx.com/sign-up');
   })
 
   it('Should SignUp with empty credentials', () => {
-    SignUpPage.signUpWithEmptyCredentials();
+    SignUpPage.fillSignUpWithEmptyCredentials();
     cy.url().should('eq','https://telnyx.com/sign-up');
   })
 
   it('Should SignUp with LogIn link button ', () => {
     SignUpPage.signUpWithLogInLinkButton();
-    LogInPage.logInSignUpLinkBtn('foloxov@gmail.com','ZpPQ_q!!u#Lz6PZ');
+    LogInPage.clickLogInSignUpLinkBtn('foloxov@gmail.com','ZpPQ_q!!u#Lz6PZ');
     LogInPage.logInErrorConfirmEmail().should('be.visible');
   })
 
   it('Should LogIn with valid credentials', () => {
-    LogInPage.logInWithValidCredentials('foloxov@gmail.com','ZpPQ_q!!u#Lz6PZ');
+    LogInPage.fillLogInWithValidCredentials('foloxov@gmail.com','ZpPQ_q!!u#Lz6PZ');
     LogInPage.logInErrorConfirmEmail().should('be.visible');
   })
 
   it('Should LogIn with invalid password', () => {
-    LogInPage.logInWithInvalidPassword('foloxov@gmail.com',"!!!!");
+    LogInPage.fillLogInWithInvalidPassword('foloxov@gmail.com',"!!!!");
     LogInPage.logInErrorConfirmEmail().should('be.visible');
   })
 
   it('Should LogIn with empty credentials', () => {
-    LogInPage.logInWithEmptyCredentials();
+    LogInPage.fillLogInWithEmptyCredentials();
     cy.url().should('include','/login/sign-in');
   })
 
   it('Should LogIn with false company email in Single Sign-On', () => {
-    LogInPage.logInCompanyEmail('foloxov@gmail.com');
+    LogInPage.fillLogInCompanyEmail('foloxov@gmail.com');
     LogInPage.logInErrorRequestNotFound().should('be.visible');
   })
 
   it('Should LogIn with false business name in Single Sign-On', () => {
-    LogInPage.logInBusinessName("5555");
+    LogInPage.fillLogInBusinessName("5555");
     LogInPage.logInErrorRequestNotFound().should('be.visible');
   })
 
   it('Should enter VideoAPI in Products tab', () => {
-    ProductsPage.productsSearchVideoAPILink();
+    ProductsPage.clickProductsSearchVideoAPILink();
     cy.url().should('include','/login/sign-in');
   })
 })
