c0f8a51c82017cadd32bd4d097eba29f04621b90
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-10 18:06:10 +0300

    Update SignUp.page.ts

--- cypress/pageobjects/SignUp.page.ts
+++ cypress/pageobjects/SignUp.page.ts
@@ -29,7 +29,7 @@ export class SignUpPage{
     }
 
     email(){
-        return Math.random().toString(36).substring(2,8);
+        return Math.random().toString(36).substring(2,8) + "@mail.com";
     }
 
     fullname(){
