0a80ee441c2eea97a94bc12da5dba024cd38aeee
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-11 17:16:10 +0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'cypress'; 
-import allureWriter from '@shelex/cypress-allure-plugin/writer';
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 export default defineConfig({
   e2e: {
