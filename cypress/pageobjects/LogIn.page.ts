4865b8a4b8db5cbe7ff7e7e16ec16643d09c9ecf
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-12-02 17:43:19 +0300

    Update LogIn.page.ts

--- cypress/pageobjects/LogIn.page.ts
+++ cypress/pageobjects/LogIn.page.ts
@@ -1,7 +1,8 @@
 
+
 class LogInPage {
     get logInBtn(){
-     return cy.xpath("//*[@id='__next']/div[1]/header/div[1]/div/div/a[2]") 
+     return cy.xpath("//span[text()='Log In']") 
     }
 
     get emailInput(){
@@ -45,7 +46,8 @@ class LogInPage {
     }
 
     clickLogInBtn(){
-        this.logInBtn.click({force:true});
+        this.logInBtn.scrollIntoView();
+        this.logInBtn.click();
     }
 
     fillEmail(email:string){
@@ -61,6 +63,7 @@ class LogInPage {
     }
 
     logInWithEmptyCredentials(){
+        this.logInBtn.scrollIntoView();
         this.logInBtn.click();
         this.submitBtn.click();
     }
