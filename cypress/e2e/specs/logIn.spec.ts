6202f576fd83ab65a0fe9b1fd4d5a63b54e0019a
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-12-02 17:01:25 +0300

    Add files via upload

---
+++ cypress/e2e/specs/logIn.spec.ts
@@ -0,0 +1,48 @@
+import LogInPage from "../../pageobjects/LogIn.page"
+import MainPage from "../../pageobjects/MainPage.page"
+
+describe('Telnyx Login Test', () => {
+  beforeEach(() => {
+    MainPage.visitUrl();
+  })
+
+  it('Should LogIn with valid credentials', () => {
+    MainPage.clickAcceptCookiesBtn();
+    LogInPage.clickLogInBtn();
+    LogInPage.fillEmail(Cypress.env('Email'));
+    LogInPage.fillPassword(Cypress.env('Password'));
+    LogInPage.clickSubmitBtn();
+    LogInPage.logInError.should('be.visible');
+  })
+
+  it('Should LogIn with invalid password with 4 symbols', () => {
+    LogInPage.clickLogInBtn();
+    LogInPage.fillEmail(Cypress.env('Email'));
+    LogInPage.fillPassword(Cypress.env('WrongPasswordWithFourSymbols'));
+    LogInPage.clickSubmitBtn();
+    LogInPage.logInError.should('be.visible');
+  })
+
+  it('Should LogIn with empty credentials', () => {
+    LogInPage.logInWithEmptyCredentials();
+    cy.url().should('include','/login/sign-in');
+  })
+
+  it('Should LogIn false company email without @ in Single Sign-On', () => {
+    LogInPage.clickLogInBtn();
+    LogInPage.clickSingleSignOnBtn();
+    LogInPage.fillCompanyEmail(Cypress.env('CompanyEmailWithout@'));
+    LogInPage.clickContinueBtn();
+    LogInPage.logInErrorRequestNotFound.should('be.visible');
+  })
+
+  it('Should LogIn false business name with four numbers in Single Sign-On', () => {
+    LogInPage.clickLogInBtn();
+    LogInPage.clickSingleSignOnBtn();
+    LogInPage.clickCompanyName();
+    LogInPage.fillBusinessName(Cypress.env('BusinessNameWithFourNumbers'));
+    LogInPage.clickContinueBtn();
+    LogInPage.logInErrorRequestNotFound.should('be.visible');
+  })
+
+})
---
+++ cypress/pageobjects/LogIn.page.ts
@@ -0,0 +1,88 @@
+
+class LogInPage {
+    get logInBtn(){
+     return cy.xpath("//*[@id='__next']/div[1]/header/div[1]/div/div/a[2]") 
+    }
+
+    get emailInput(){
+     return cy.get("form>div>div>label>span~div>div>input[name='email']")
+    }
+
+    get passwordInput(){
+     return cy.get("input[name='password']")
+    }
+
+    get submitBtn(){
+     return cy.xpath("//button[text()='Log in']")
+    }
+
+    get logInError(){
+     return cy.get("div[type='error']",{ timeout: 5000 })
+    }
+
+    get singleSignOnBtn(){
+     return cy.xpath("//button[text()='Single Sign-On']")
+    }
+
+    get businessNameInput(){
+     return cy.get("input[name='short_name']")
+    }
+
+    get companyEmailInput(){
+     return cy.get("input[placeholder='e.g. jane@acme.com']")
+    }
+
+    get continueBtn(){
+     return cy.xpath("//button[text()='Continue']")
+    }
+
+    get companyNameBtn(){
+     return cy.xpath("//button[text()='company name']")
+    }
+
+    get logInErrorRequestNotFound(){
+     return cy.get("div[type='error']")
+    }
+
+    clickLogInBtn(){
+        this.logInBtn.click();
+    }
+
+    fillEmail(email:string){
+        this.emailInput.type(email);
+    }
+
+    fillPassword(password:string){
+        this.passwordInput.type(password);
+    }
+
+    clickSubmitBtn(){
+        this.submitBtn.click();
+    }
+
+    logInWithEmptyCredentials(){
+        this.logInBtn.click();
+        this.submitBtn.click();
+    }
+
+    clickSingleSignOnBtn(){
+        this.singleSignOnBtn.click();
+    }
+
+    fillCompanyEmail(email:string){
+        this.companyEmailInput.type(email);
+    }
+
+    clickCompanyName(){
+        this.companyNameBtn.click();     
+    }
+
+    fillBusinessName(name:string){
+        this.businessNameInput.type(name);
+    }
+
+    clickContinueBtn(){
+        this.continueBtn.click();
+    }
+}
+export default new LogInPage();
